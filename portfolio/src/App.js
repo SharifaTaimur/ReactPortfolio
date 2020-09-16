6ecad7ae690a32b3c68f48fd248b9702e90d4a62
Sharifa Taimur <sharifa_taimur2@hotmail.com>
2020-09-16 23:40:56 +0400

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   getResumeData() {
     $.ajax({
-      url: "/profileupdate/resumeData.json",
+      url: "/profileupdate/public/resumeData.json",
       dataType: "json",
       cache: false,
       success: function (data) {
