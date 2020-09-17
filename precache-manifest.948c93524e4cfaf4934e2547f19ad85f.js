e321466eb9fd74a089df45a4491ed0f81de9a812
SharifaTaimur <sharifa_taimur2@hotmail.com>
2020-09-18 02:37:03 +0400

    Updates

---
+++ precache-manifest.948c93524e4cfaf4934e2547f19ad85f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dd5aad97c683fea09c25fe0708e755",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "602d42a0a007301137c3",
+    "url": "/reactportfolio/static/css/main.bc758f60.chunk.css"
+  },
+  {
+    "revision": "282887f4b3c4157648e8",
+    "url": "/reactportfolio/static/js/2.db423ff4.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/reactportfolio/static/js/2.db423ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602d42a0a007301137c3",
+    "url": "/reactportfolio/static/js/main.085771e4.chunk.js"
+  },
+  {
+    "revision": "ed2a1300382c2d85a850",
+    "url": "/reactportfolio/static/js/runtime-main.bc6e5880.js"
+  }
+]);
